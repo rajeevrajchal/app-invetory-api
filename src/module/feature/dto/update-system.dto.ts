58ee005af2db34b7485d6e1117228cc3f4c24a77
Rajeev Rajchal <rajeevrajchal12@gmail.com>
2024-04-02 22:53:17 +0545

    Bugfix: Fixed Build error

--- src/module/feature/dto/update-system.dto.ts
+++ src/module/feature/dto/update-system.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateSystemDto } from './create-feature.dto';
+import { CreateFeatureDto } from './create-feature.dto';
 
-export class UpdateSystemDto extends PartialType(CreateSystemDto) {}
+export class updateFeatureDto extends PartialType(CreateFeatureDto) {}
